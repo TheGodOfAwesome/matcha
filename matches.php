c2b010fb0ed29e4c266ecbf36ac28d43b90ec100
Kuzivakwashe MUVEZWA <kmuvezwa@e4r6p8.wethinkcode.co.za>
2017-12-11 18:34:50 +0200

    matches now show basic matches

--- matches.php
+++ matches.php
@@ -18,12 +18,34 @@ if($name == "" || $email == "" || $status != "logged in")
     exit();
 }
 
+$path = "./images/user_images/";
+$type = "profile";
+$stmt_profile = $conn->prepare("SELECT * FROM users WHERE name=:name");
+$stmt_profile->bindValue(":name", $name);
+// initialise an array for the results 
+$user = array();
+if ($stmt_profile->execute()) {
+  while ($row = $stmt_profile->fetch(PDO::FETCH_ASSOC)) {
+    $user[] = $row;
+    $full_name = $row['fullname'];
+    $user_name = $row['name'];
+    $user_dob = $row['dateofbirth'];
+    $user_bio = $row['bio'];
+    $user_gender = $row['gender'];
+    $user_preference = $row['preference'];
+    $user_city = $row['city'];
+    $user_country = $row['country'];
+    $user_loginstatus = $row['loginstatus'];
+    $user_lastseen = $row['lastseen'];
+  }
+}
+
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-<title>Matcha - Home</title>
+<title>Matcha - Matches</title>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -70,26 +92,34 @@ if($name == "" || $email == "" || $status != "logged in")
     <h2>Matches</h2>
     <div class="col-lg-8 col-md-8 col-sm-8">
         <div class="left_content">
-          <div style="height:350px; width:100%; overflow:hidden; border:10px solid;">
+          <div style="height:350px; width:100%; overflow: auto; border:10px solid;">
 
           <?php  
 
-            $log_action_result = 0;
-            $stmt_log = $conn->prepare("SELECT * FROM log WHERE log_action_recipient_name=:log_action_recipient_name AND log_action_result=:log_action_result ORDER BY log_timestamp DESC");
-            $stmt_log->bindValue(":log_action_recipient_name", $name);
-            $stmt_log->bindValue(":log_action_result", $log_action_result);
-            if ($stmt_log->execute()) {
-            while ($row = $stmt_log->fetch(PDO::FETCH_ASSOC)) {
-              $log_id = $row['log_id'];
-              $log_action = $row['log_action'];
-              $log_creator = $row['log_user_name'];
+            $status = "activated";
+            $stmt_matches = $conn->prepare("SELECT * FROM users WHERE city=:city AND country=:country");
+            $stmt_matches->bindValue(":city", $user_city);
+            $stmt_matches->bindValue(":country", $user_country);
+            if ($stmt_matches->execute()) {
+            while ($row = $stmt_matches->fetch(PDO::FETCH_ASSOC)) {
+              $match_id = $row['id'];
+              $matchfull_name = $row['fullname'];
+              $match_name = $row['name'];
+              $match_dob = $row['dateofbirth'];
+              $match_bio = $row['bio'];
+              $match_gender = $row['gender'];
+              $match_preference = $row['preference'];
+              $match_city = $row['city'];
+              $match_country = $row['country'];
+              $match_loginstatus = $row['loginstatus'];
+              $match_lastseen = $row['lastseen'];
 
                     /******************************************************************************** */
 
                     $path = "./images/user_images/";
                     $type = "profile";
                     $stmt_notificationprofilephoto = $conn->prepare("SELECT * FROM images WHERE image_creator=:image_creator AND image_type=:image_type ORDER BY image_timestamp DESC");
-                    $stmt_notificationprofilephoto->bindValue(":image_creator", $log_creator);
+                    $stmt_notificationprofilephoto->bindValue(":image_creator", $match_name);
                     $stmt_notificationprofilephoto->bindValue(":image_type", $type);
                     // initialise an array for the results 
                     $user_image = array();
@@ -103,7 +133,7 @@ if($name == "" || $email == "" || $status != "logged in")
 
                     /******************************************************************************** */
 
-                    If ($log_action == "like") {
+                    /*If ($log_action == "like") {
                       $counter_action = "like back";
                       echo '
                         <div class="media"> <a href="inc/likeback.php?id=' . $log_id . '&profile=' . $log_creator . '" class="media-left"> <img src=" ' . $url . '" alt=""> </a>
@@ -124,10 +154,49 @@ if($name == "" || $email == "" || $status != "logged in")
                           <div class="media-body"> <a href="inc/message.php?profile=' . $log_creator . '" class="catg_title">Message ' . $log_creator . ' Back</a> </div>
                         </div>
                           ';
+                    }*/
+
+                    if ($match_dob != ""){
+                      //date is in yyyy/mm/dd format;
+                      //explode the date to get month, day and year
+                      $birthDate = explode("-", $user_dob);
+                      //get age from date or birthdate
+                      $age = (date("md", date("U", mktime(0, 0, 0, $birthDate[1], $birthDate[2], $birthDate[0]))) > date("md")
+                      ? ((date("Y") - $birthDate[0]) - 1)
+                      : (date("Y") - $birthDate[0]));
+                      $user_age = $age;
                     }
+                    
+                    if ($match_name == $name){
+
+                    } else if ($match_city != $user_city){
+
+                    } else if ($match_country != $user_country){
+
+                    }/* else if ($match_dob == ""){
+
+                    }*/ 
+                    else {
+                      if ($user_preference == "" && $user_preference == "undefined"){
+                        echo '
+                        <div class="media"> <a href="./profile.php?profile=' . $match_name . '" class="media-left"> <img src=" ' . $url . '" alt=""> </a>
+                          <div class="media-body"> <a href="./profile.php?profile=' . $match_name . '" class="catg_title"><h6>' . $match_name . ' is a potential connection for you!</a></h6><a href="./profile.php?profile=' . $match_name . '" ><h2>View ' . $match_name . '</h2></a></div>
+                        </div>
+                        ';
+                      }else{
+                        if ($user_preference == $match_gender && ($match_preference == "undefined" || $match_preference == $user_gender)){
+                          echo '
+                          <div class="media"> <a href="./profile.php?profile=' . $match_name . '" class="media-left"> <img src=" ' . $url . '" alt=""> </a>
+                            <div class="media-body"> <a href="./profile.php?profile=' . $match_name . '" class="catg_title"><h6>' . $match_name . ' is a potential connection for you!</a></h6><a href="./profile.php?profile=' . $match_name . '" ><h2>View ' . $match_name . '</h2></a></div>
+                          </div>
+                          ';
+                        }
+                      }
+                    }
+
                   }
                 }
-              ?>
+            ?>
 
           </div>
           <div class="contact_area">
